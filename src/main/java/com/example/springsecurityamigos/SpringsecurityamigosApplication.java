84cc215fddbb0823e7d69090a74a850823584534
filmboyNationwide <james.stewart@nationwide.co.uk>
2022-12-06 08:54:30 +0000

    First Commit - API working

---
+++ src/main/java/com/example/springsecurityamigos/SpringsecurityamigosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityamigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityamigosApplication.class, args);
+	}
+
+}
