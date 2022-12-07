4d51c576e9310cf7e1116d9d9bef02405897d79f
filmboyNationwide <james.stewart@nationwide.co.uk>
2022-12-07 11:57:46 +0000

    Up to Role Based Authentication

--- src/main/java/com/example/springsecurityamigos/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurityamigos/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecurityamigos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,13 +9,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static com.example.springsecurityamigos.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalAuthentication
 public class ApplicationSecurityConfig{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
@@ -22,11 +33,28 @@ public class ApplicationSecurityConfig{
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> {
                     auth.requestMatchers("/","index","/css/*", "/js/*").permitAll();
-//                    auth.requestMatchers("/api/**").hasRole(STUDENT.name());
-                    auth.requestMatchers("/admin").hasRole("ADMIN");
+                   auth.requestMatchers("/api/**").authenticated();
+                    auth.requestMatchers("/admin").authenticated();
                 })
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
 
 
-}}
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())  //ROLE_STUDENT
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name()) //ROLE_ADMIN
+                .build();
+
+        return new InMemoryUserDetailsManager(annaSmithUser,lindaUser);
+    }
+}
---
+++ src/main/java/com/example/springsecurityamigos/security/ApplicationUserRole.java
@@ -0,0 +1,26 @@
+package com.example.springsecurityamigos.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.example.springsecurityamigos.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(
+            COURSE_READ,
+            COURSE_WRITE,
+            STUDENT_READ,
+            STUDENT_WRITE
+    ));
+
+    private final Set<ApplicationUserPermission> permissions;
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+}
+
